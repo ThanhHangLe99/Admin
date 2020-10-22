4b6a4045f6ee50e9d67d79b879431b147081cac2
ThanhHangLe99 <70497427+ThanhHangLe99@users.noreply.github.com>
2020-10-22 23:38:28 +0700

    update

--- src/app/main/product/product.module.ts
+++ src/app/main/product/product.module.ts
@@ -1,32 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { ProductComponent } from './product/product.component';
 import { TypeComponent } from './type/type.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FileUploadModule } from 'primeng/fileupload';
+
+const routes: Routes = [
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  { path: 'category', component: TypeComponent },
+  { path: 'order', component: OrderComponent}
+];
 
 @NgModule({
-  declarations: [ 
+  declarations: [
     OrderComponent,ProductComponent,TypeComponent
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     SharedModule,
-    RouterModule.forChild([
-      {
-        path: 'order',
-        component: OrderComponent,
-      },
-      {
-        path: 'product',
-        component: ProductComponent,
-      },
-      {
-        path: 'type',
-        component: TypeComponent,
-      },
-  ]),  
+    ReactiveFormsModule,
+    FileUploadModule,
+    SharedModule,
   ]
 })
 export class ProductModule { }
