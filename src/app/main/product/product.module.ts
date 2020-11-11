b77860587ad1802f0f9605f9bac1cb699fdd75d4
ThanhHangLe99 <70497427+ThanhHangLe99@users.noreply.github.com>
2020-11-11 16:32:05 +0700

    update

--- src/app/main/product/product.module.ts
+++ src/app/main/product/product.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,19 +8,29 @@ import { TypeComponent } from './type/type.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FileUploadModule } from 'primeng/fileupload';
+import { HttpClientModule} from '@angular/common/http';
+import {EditorModule} from 'primeng/editor';
+import { BrandComponent } from './brand/brand.component';
 
 const routes: Routes = [
   {
     path: 'product',
     component: ProductComponent
   },
-  { path: 'category', component: TypeComponent },
-  { path: 'order', component: OrderComponent}
+  {
+    path: 'category',
+    component: TypeComponent
+  },
+  {
+    path: 'brand',
+    component: BrandComponent
+  },
+  { path: 'order', component: OrderComponent},
 ];
 
 @NgModule({
   declarations: [
-    OrderComponent,ProductComponent,TypeComponent
+    OrderComponent,ProductComponent,TypeComponent, BrandComponent, BrandComponent
   ],
   imports: [
     CommonModule,
@@ -28,6 +39,7 @@ const routes: Routes = [
     ReactiveFormsModule,
     FileUploadModule,
     SharedModule,
-  ]
+    HttpClientModule,
+    EditorModule  ]
 })
 export class ProductModule { }
