dd7985a6dda4149a5f1f4a4b28b582774c78c959
ThanhHangLe99 <70497427+ThanhHangLe99@users.noreply.github.com>
2020-12-08 14:15:20 +0700

    update

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import * as CanvasJS from './canvasjs.min';
-//var CanvasJS = require('./canvasjs.min');
+import * as CanvasJS from '../../../assets/js/canvasjs/canvasjs.min.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,28 +11,25 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  //   let chart = new CanvasJS.Chart("chartContainer", {
-	// 	animationEnabled: true,
-	// 	exportEnabled: true,
-	// 	title: {
-	// 		text: "Basic Column Chart in Angular"
-	// 	},
-	// 	data: [{
-	// 		type: "column",
-	// 		dataPoints: [
-	// 			{ y: 71, label: "Apple" },
-	// 			{ y: 55, label: "Mango" },
-	// 			{ y: 50, label: "Orange" },
-	// 			{ y: 65, label: "Banana" },
-	// 			{ y: 95, label: "Pineapple" },
-	// 			{ y: 68, label: "Pears" },
-	// 			{ y: 28, label: "Grapes" },
-	// 			{ y: 34, label: "Lychee" },
-	// 			{ y: 14, label: "Jackfruit" }
-	// 		]
-	// 	}]
-	// });
-
-	// chart.render();
+    let chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		exportEnabled: true,
+		title: {
+			text: ""
+		},
+		data: [{
+			type: "column",
+			dataPoints: [
+        { y: 71, label: "Áo" },
+        { y: 60, label: "Quần" },
+				{ y: 95, label: "Váy đầm" },
+				{ y: 80, label: "Đồ bộ" },
+				{ y: 65, label: "LV" },
+				{ y: 95, label: "Dior" },
+				{ y: 68, label: "Chanel" },
+			]
+		}]
+	});
+	chart.render();
    }
 }
